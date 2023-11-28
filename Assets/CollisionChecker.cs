a1060db9bf2b7ed8883f76788410ed24c6e24e0c
der_Kirschbaum <48173706+derKirschbaum@users.noreply.github.com>
2023-11-28 17:21:58 +0700

    Initial with some test

---
+++ Assets/CollisionChecker.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("I hitted the " + collision.body.name);
+    }
+
+}
